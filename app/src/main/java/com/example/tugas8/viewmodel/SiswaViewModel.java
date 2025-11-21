a6421824640eabc082bc320561144be87ca332aa
Refinaldii <refinaldi9786@gmail.com>
2025-11-21 10:22:36 +0700

    Bikin val status

--- app/src/main/java/com/example/tugas8/viewmodel/SiswaViewModel.java
+++ app/src/main/java/com/example/tugas8/viewmodel/SiswaViewModel.java
@@ -2,5 +2,6 @@ package com.example.tugas8.viewmodel;
 
 class SiswaViewModel : ViewModel() {
     private val _statusUI = MutableStateFlow(Siswa())
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
 
 }
